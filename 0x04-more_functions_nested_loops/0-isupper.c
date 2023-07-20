76df9b9b8e45567ce6c160770fed4b204f23a87e
D.joseph21 <dennisjoseph927@gmail.com>
2023-07-20 03:13:13 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+int _putchar(ch)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
