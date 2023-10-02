f2756667b4e17480b59ad97052ff27215042d96e
D-joseph21 <dennisjoseph927@gmail.com>
2023-10-02 08:03:55 -0700

    To infinity and beyond

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,6 +11,6 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", *argv); 
-return (0); 
+printf("%s\n", *argv);
+return (0);
 }
